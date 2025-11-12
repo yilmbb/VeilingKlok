68fae882f98cb59eb7790e9f56541919ae36c59b
mazlumaslan1 <127404266+mazlumaslan1@users.noreply.github.com>
2025-11-12 16:44:39 +0100

    added Gebruiker.cs

---
+++ VeilingKlok/VeilingKlok/Models/Gebruiker.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VeilingKlok.Models;
+
+// PK als int; Identity dekt gebruikersnaam/wachtwoord/email
+public class Gebruiker : IdentityUser<int>
+{
+    // Gedeeld door Koper & Verkoper
+    public string? Bedrijfsnaam { get; set; }
+    public string? KvK { get; set; }
+    public string? BtwNummer { get; set; }
+    public string? Telefoon { get; set; }
+    public string? RekeningNummer { get; set; }
+
+    // Alleen relevant voor Koper
+    public string? Leveradres { get; set; }
+
+    // Alleen relevant voor Verkoper
+    public decimal? Totaalomzet { get; set; }
+}
